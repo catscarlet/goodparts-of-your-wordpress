3bc5759910548b554ef0636b4467e36a367aca8d
catscarlet <i@catscarlet.com>
2017-11-27 01:10:30 +0800

    增加 HTTP_REFERER 的日志记录，以及跨域和防盗链代码

--- api/get_content.php
+++ api/get_content.php
@@ -3,6 +3,19 @@
 include_once 'config.php';
 include_once 'logrecorder.php';
 
+/*
+if (array_key_exists('HTTP_REFERER', $_SERVER) && 'articles.catscarlet.com' != parse_url($_SERVER['HTTP_REFERER'])['host']) {
+    header('HTTP/1.0 403 Forbidden');
+    $result = [];
+    $result['result'] = 1;
+    $result['content'] = $_SERVER['HTTP_REFERER'];
+    $rst = json_encode($result, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+
+    echo $rst;
+    exit();
+}
+*/
+
 $id = $_GET['id'];
 
 $query = "SELECT post_content FROM `wp_posts` WHERE post_status = 'publish' and ID = $id";
--- api/logrecorder.php
+++ api/logrecorder.php
@@ -3,10 +3,11 @@
 $log['REQUEST_TIME'] = date('c', $_SERVER['REQUEST_TIME']);
 $log['REQUEST_URI'] = $_SERVER['REQUEST_URI'];
 $log['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
+$log['HTTP_REFERER'] = $_SERVER['HTTP_REFERER'];
 $log['HTTP_USER_AGENT'] = $_SERVER['HTTP_USER_AGENT'];
 
 $rst = json_encode($log, JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES);
 
-$path = '/var/www/html/goodstuff/api/api.log';
+$path = './api.log';
 
 //file_put_contents($path, $rst."\n", FILE_APPEND);
