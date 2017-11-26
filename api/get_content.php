37e0cd117ffe990df01a22270d4f38283cf6b99d
catscarlet <i@catscarlet.com>
2017-11-27 01:17:57 +0800

    增加对请求无id参数或参数错误的400和404处理

--- api/get_content.php
+++ api/get_content.php
@@ -15,13 +15,31 @@ if (array_key_exists('HTTP_REFERER', $_SERVER) && 'articles.catscarlet.com' != p
     exit();
 }
 */
+if (!array_key_exists('id', $_GET)) {
+    header('HTTP/1.0 400 Bad Request');
+    $result = [];
+    $result['result'] = 400;
+    $result['content'] = $_GET;
+    $rst = json_encode($result, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES);
 
-$id = $_GET['id'];
+    echo $rst;
+    exit();
+} else {
+    $id = $_GET['id'];
+}
 
 $query = "SELECT post_content FROM `wp_posts` WHERE post_status = 'publish' and ID = $id";
 $result = $mysqli->query($query);
 
-if ($row = $result->fetch_assoc()) {
+if (!($row = $result->fetch_assoc())) {
+    header('HTTP/1.0 404 Not Found');
+    $result = [];
+    $result['result'] = 404;
+    $result['content'] = $_GET;
+    $rst = json_encode($result, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES);
+
+    echo $rst;
+    exit();
 }
 
 $result = [];
