91de21e9bb75ca99c2bc1b7d37333c8fb7ca3df0
catscarlet <i@catscarlet.com>
2017-11-27 11:27:11 +0800

    修复日志记录 Undefined index: HTTP_REFERER 问题

--- api/logrecorder.php
+++ api/logrecorder.php
@@ -3,7 +3,9 @@
 $log['REQUEST_TIME'] = date('c', $_SERVER['REQUEST_TIME']);
 $log['REQUEST_URI'] = $_SERVER['REQUEST_URI'];
 $log['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
-$log['HTTP_REFERER'] = $_SERVER['HTTP_REFERER'];
+if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+    $log['HTTP_REFERER'] = $_SERVER['HTTP_REFERER'];
+}
 $log['HTTP_USER_AGENT'] = $_SERVER['HTTP_USER_AGENT'];
 
 $rst = json_encode($log, JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES);
