25a5d22cd32d716ed56e6a80bf408394d5b99d51
catscarlet <i@catscarlet.com>
2018-10-20 15:37:01 +0800

    Update allowed_referers and add more information to handle bad request.

--- api/config.php
+++ api/config.php
@@ -1,7 +1,7 @@
 <?php
 
-
-$mysqli = new mysqli('127.0.0.1', 'your_account', 'your_password', 'your_wp_database');
+$allowed_referers = ['articles.catscarlet.com', '192.168.1.101'];
+$mysqli = new mysqli('127.0.0.1', 'YOUR_WP_DATABASE_ACCOUNT', 'YOUR_WP_DATABASE_ACCOUNT', 'YOUR_WP_DATABASE');
 $mysqli->set_charset('utf8mb4');
 
 if ($mysqli->connect_errno) {
